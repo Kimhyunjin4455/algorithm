a844275bb20612bdcef07dcd3cb563b83590b3c9
Kimhyunjin4455 <dp4455@naver.com>
2024-03-25 23:33:50 +0900

    Solve baekJoon2805

--- src/main/java/org/example/algorithmSolution/problem/RankSearch.java
+++ src/main/java/org/example/algorithmSolution/problem/RankSearch.java
@@ -1,5 +1,92 @@
 package org.example.algorithmSolution.problem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class RankSearch {
+
+    public static void main(String[] args) {
+        String[] info = {"java backend junior pizza 150",
+                "python frontend senior chicken 210",
+                "python frontend senior chicken 150",
+                "cpp backend senior pizza 260",
+                "java backend junior chicken 80",
+                "python backend senior chicken 50"};
+        String[] query = {"java and backend and junior and pizza 100",
+                "python and frontend and senior and chicken 200",
+                "cpp and - and senior and pizza 250",
+                "- and backend and senior and - 150",
+                "- and - and - and chicken 100",
+                "- and - and - and - 150"};
+
+        int[] sol = solution(info, query);
+        for(int i=0; i< sol.length; i++){
+            if(i==0){
+                System.out.print("["+sol[i]+",");
+            }else if(i== sol.length-1){
+                System.out.print(sol[i]+"]");
+            }else System.out.print(sol[i]+",");
+        }
+
+    }
+
+
+
+    public static int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        // info 기반 hashMap 생성
+        HashMap<String, ArrayList<Integer>> hashMap =  new HashMap<>();
+
+        for(String i: info){
+            String[] data = i.split(" ");
+            String[] languages = {data[0], "-"};
+            String[] jobs = {data[1], "-"};
+            String[] exps = {data[2], "-"};
+            String[] foods = {data[3], "-"};
+            int value = Integer.parseInt(data[4]);
+
+            for(String lang: languages){
+                for (String job: jobs){
+                    for (String exp: exps){
+                        for (String food: foods){
+                            String[] keyData = {lang, job, exp, food};
+                            String key = String.join(" ", keyData);
+                            ArrayList<Integer> arr = hashMap.getOrDefault(key, new ArrayList<Integer>());
+                            // 있으면 key에 상응하는 ArrayList, 없으면 빈 리스트 생성
+                            arr.add(value);
+                            hashMap.put(key, arr);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (ArrayList<Integer> arr: hashMap.values()) arr.sort(null);
+
+        int idx=0;
+        for(String q: query){
+            String[] data = q.split(" and ");
+            int target = Integer.parseInt(data[3].split(" ")[1]); // ex: "chicken 150"에서 150만 분리
+            data[3] = data[3].split(" ")[0]; // 음식만 분리
+            String key = String.join(" ", data);
+
+            if(hashMap.containsKey(key)){
+                ArrayList<Integer> list = hashMap.get(key);
+                int start = 0;
+                int end = list.size()-1;
+                while(start<=end){
+                    int mid = (start+end)/2;
+                    if(list.get(mid) >= target){
+                        end = mid-1;
+                    }else start = mid+1;
+                }
+                answer[idx] = list.size()-start;
+            }
+            idx++;
+        }
+        return answer;
+    }
+
     
 }
---
+++ src/main/java/org/example/binarySearch/BinarySearch2805.java
@@ -0,0 +1,58 @@
+package org.example.binarySearch;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BinarySearch2805 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> trees = new ArrayList<>();
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++){
+            trees.add(Integer.parseInt(st.nextToken()));
+        }
+
+        trees.sort(null);
+
+        int start = 0;
+        int end = trees.get(trees.size()-1);
+
+
+        int result = bs(start, end, trees, M);
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static int bs(int start, int end, ArrayList<Integer> trees, int M) {
+        int mid = (start + end) / 2;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            long sum = 0;
+            for (int length : trees) {
+                if (length > mid) {
+                    sum += (length - mid);
+                }
+            }
+            //System.out.println("s: "+sum);
+            if (sum >= M) { // sum이 더 커져야 함 -> mid값이 더 작아져야 함
+                start = mid +1;
+            }else if(sum < M) end = mid-1;
+
+        }return end;
+    }
+}
